f24914504a3c62bf358e596d499d26deb2eac60c
Brandon Mrsly <bmrsny@gmail.com>
2016-01-13 11:54:41 -0700

    finished ch6

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  // namespace: 'api'
+  namespace: 'api'
   // hostname: 'https://embernote.example.com' // maybe you have a dif subdomain used for api call
-  namespace: 'api/alt',
+  // namespace: 'api/alt',
 
   headers: function() {
     if(this.get('session.user')) {
