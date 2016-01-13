447929ba89f172b86defa8228ab8dd5b0649a53e
Brandon Mrsly <bmrsny@gmail.com>
2016-01-13 16:10:16 -0700

    started to learn how to test models and now components

--- tests/unit/models/notebook-test.js
+++ tests/unit/models/notebook-test.js
@@ -1,8 +1,12 @@
 import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleForModel('notebook', 'Unit | Model | notebook', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: [
+    'model:user',
+    'model:note'
+  ]
 });
 
 test('it exists', function(assert) {
@@ -10,3 +14,18 @@ test('it exists', function(assert) {
   // let store = this.store();
   assert.ok(!!model);
 });
+
+test('it counts notes', function(assert) {
+    var notebook = this.subject({title: 'my title'});
+    var note;
+    var noteCount = Math.floor(Math.random() * (10 - 1) + 1);
+
+    Ember.run(() => {
+      for(var i = 0; i < noteCount; i++) {
+       note = this.store().createRecord('note'); 
+       notebook.get('notes').addObject(note);
+      }
+    });
+
+    assert.equal(notebook.noteCount(), noteCount);
+  });
--- tests/unit/serializers/application-test.js
+++ tests/unit/serializers/application-test.js
@@ -1,15 +1,14 @@
 import { moduleForModel, test } from 'ember-qunit';
 
-moduleForModel('application', 'Unit | Serializer | application', {
+moduleForModel('user', 'Unit | Serializer | application', {
   // Specify the other units that are required for this test.
-  needs: ['serializer:application']
+  needs: ['model:user']
 });
-
 // Replace this with your real tests.
 test('it serializes records', function(assert) {
-  let record = this.subject();
+  var record = this.subject();
 
-  let serializedRecord = record.serialize();
+  var serializedRecord = record.serialize();
 
   assert.ok(serializedRecord);
 });
